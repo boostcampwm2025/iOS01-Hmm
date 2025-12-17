7c3a44dd405377962e5c3ef92d7be5af269e258d
snughnu <ksnughnu@gmail.com>
2025-12-18 00:10:49 +0900

    feat: MotionManager 구현

---
+++ Prototype/Prototype/Prototype/Motion.swift
@@ -0,0 +1,75 @@
+//
+//  Motion.swift
+//  Prototype
+//
+//  Created by 김성훈 on 12/17/25.
+//
+
+import CoreMotion
+import SwiftUI
+
+// MARK: - MotionManager
+@Observable
+class MotionManager {
+    private let motionManager = CMMotionManager()
+
+    // 설정값
+    private let updateInterval = 1.0 / 60.0      // 주사율 16.67 ms
+    private let threshold: Double = 0.05         // 데드존 (무반응 구간)
+    private let movementSpeed: CGFloat = 1000.0  // 곱할 속도 (민감도)
+
+    // View에서 사용할 데이터들
+    var gravityX: Double = 0
+    var gravityY: Double = 0
+    var gravityZ: Double = 0
+    var roll: Double = 0
+    var pitch: Double = 0
+    var yaw: Double = 0
+    var userAccelX: Double = 0
+    var userAccelY: Double = 0
+    var userAccelZ: Double = 0
+    var rotationX: Double = 0
+    var rotationY: Double = 0
+    var rotationZ: Double = 0
+    var characterX: CGFloat = 0
+
+    init() { startMotionUpdates() }
+    deinit { motionManager.stopDeviceMotionUpdates() }
+
+    func startMotionUpdates() {
+        guard motionManager.isDeviceMotionAvailable else { return }
+        motionManager.deviceMotionUpdateInterval = updateInterval
+
+        motionManager.startDeviceMotionUpdates(to: .main) { [weak self] motion, error in
+            guard let self = self, let motion = motion else { return }
+
+            self.gravityX = motion.gravity.x
+            self.gravityY = motion.gravity.y
+            self.gravityZ = motion.gravity.z
+
+            // 라디안 -> 각도 변환
+            self.roll = motion.attitude.roll * 180 / .pi
+            self.pitch = motion.attitude.pitch * 180 / .pi
+            self.yaw = motion.attitude.yaw * 180 / .pi
+
+            self.userAccelX = motion.userAcceleration.x
+            self.userAccelY = motion.userAcceleration.y
+            self.userAccelZ = motion.userAcceleration.z
+
+            self.rotationX = motion.rotationRate.x
+            self.rotationY = motion.rotationRate.y
+            self.rotationZ = motion.rotationRate.z
+
+            // 캐릭터 이동
+            if abs(self.gravityX) > self.threshold {
+                self.characterX += CGFloat(self.gravityX) * self.movementSpeed * CGFloat(self.updateInterval)
+
+                // 화면 밖 방지
+                let screenLimit: CGFloat = 150
+                self.characterX = max(-screenLimit, min(screenLimit, self.characterX))
+            }
+        }
+    }
+
+    func recalibrate() { }
+}
