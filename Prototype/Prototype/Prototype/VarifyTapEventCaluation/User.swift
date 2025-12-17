febaaafa4d872645fb44da9209ec90968e60e8c2
JunYoung <ghddns34@gmail.com>
2025-12-17 17:44:44 +0900

    feature: user model 추가

---
+++ Prototype/Prototype/Prototype/VarifyTapEventCaluation/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Prototype
+//
+//  Created by SeoJunYoung on 12/17/25.
+//
+
+actor User {
+    /// 닉네임
+    let nickname: String = "ProtoType"
+    
+    /// 재화 지갑
+    private(set) var wallet: Wallet = .init(
+        money: .init(amount: 0),
+        diamond: .init(amount: 0)
+    )
+}
