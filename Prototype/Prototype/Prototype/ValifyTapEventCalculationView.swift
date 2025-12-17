95ef9bd85b591139e2d1926699bf2936f95476bd
JunYoung <ghddns34@gmail.com>
2025-12-17 10:56:03 +0900

    feature: valifyTapEventCalculationView 파일 추가

---
+++ Prototype/Prototype/Prototype/ValifyTapEventCalculationView.swift
@@ -0,0 +1,18 @@
+//
+//  ValifyTapEventCalculationView.swift
+//  Prototype
+//
+//  Created by SeoJunYoung on 12/17/25.
+//
+
+import SwiftUI
+
+struct ValifyTapEventCalculationView: View {
+    var body: some View {
+        Text("hi")
+    }
+}
+
+#Preview {
+    ValifyTapEventCalculationView()
+}
