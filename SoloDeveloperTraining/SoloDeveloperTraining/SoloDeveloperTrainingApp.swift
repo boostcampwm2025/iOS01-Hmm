0dfc2ff467b91bbbd4fb71714338d6ac8ab4e228
JunYoung <ghddns34@gmail.com>
2026-01-06 18:01:13 +0900

    init: project init

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/SoloDeveloperTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloDeveloperTrainingApp.swift
+//  SoloDeveloperTraining
+//
+//  Created by SeoJunYoung on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SoloDeveloperTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
