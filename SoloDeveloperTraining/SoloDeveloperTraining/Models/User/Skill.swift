3f12bf1c85cb6d4b9905f93c241a84ea478b5577
JunYoung <ghddns34@gmail.com>
2026-01-06 22:44:56 +0900

    feat: 스킬 구체화

--- SoloDeveloperTraining/SoloDeveloperTraining/Models/User/Skill.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Models/User/Skill.swift
@@ -5,5 +5,51 @@
 //  Created by SeoJunYoung on 1/6/26.
 //
 
+import Foundation
 
-class Skill {}
+final class Skill {
+    /// 미니게임 종류
+    let game: GameKind
+    /// 스킬 등급
+    var tier: SkillTier
+    /// 스킬 레벨
+    var level: Int
+    
+    init(game: GameKind, tier: SkillTier, level: Int) {
+        self.game = game
+        self.tier = tier
+        self.level = level
+    }
+}
+
+/// 업무 종류
+enum GameKind {
+    case tap
+    case language
+    case dodge
+    case stack
+    
+    var displayTitle: String {
+        switch self {
+        case .tap: "코드짜기"
+        case .language: "언어 맞추기"
+        case .dodge: "버그 피하기"
+        case .stack: "물건 쌓기"
+        }
+    }
+}
+
+/// 스킬 등급
+enum SkillTier: Int {
+    case beginner = 0
+    case intermediate = 1
+    case advanced = 2
+    
+    var displayTitle: String {
+        switch self {
+        case .beginner: "초급"
+        case .intermediate: "중급"
+        case .advanced: "고급"
+        }
+    }
+}
