be6db5917c582a737c12862e979d8e6dbea62938
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-01-09 01:16:44 +0900

    chore: Dev Target 생성 및 컴파일 조건 플래그 설정, 타겟 멤버십 설정

--- SoloDeveloperTraining/SoloDeveloperTraining/SoloDeveloperTrainingApp.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/SoloDeveloperTrainingApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct SoloDeveloperTrainingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            #if DEV_BUILD
+            ContentView() // 이 파일이 Dev 타깃에만 있다면, 반드시 #if 안에 있어야 합니다.
+            #else
+            MainView()    // 운영 타깃용 뷰 (두 타깃 모두에 있는 파일이어야 함)
+            #endif
         }
     }
 }
