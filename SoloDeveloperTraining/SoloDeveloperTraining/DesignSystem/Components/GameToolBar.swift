93ee82f910c73bca226a950eb1c9815de7218bc4
JunYoung <ghddns34@gmail.com>
2026-01-12 22:20:29 +0900

    add: 파일 및 이미지 리소스 추가

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/GameToolBar.swift
@@ -0,0 +1,18 @@
+//
+//  GameToolBar.swift
+//  SoloDeveloperTraining
+//
+//  Created by SeoJunYoung on 1/12/26.
+//
+
+import SwiftUI
+
+struct GameToolBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameToolBar()
+}
