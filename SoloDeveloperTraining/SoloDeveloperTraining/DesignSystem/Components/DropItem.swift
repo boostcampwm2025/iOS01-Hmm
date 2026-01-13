172beda69640627dff973f9d82de505053c69972
JunYoung <ghddns34@gmail.com>
2026-01-13 11:19:40 +0900

    feat: 리소스 및 파일 추가

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/DropItem.swift
@@ -0,0 +1,18 @@
+//
+//  DropItem.swift
+//  SoloDeveloperTraining
+//
+//  Created by SeoJunYoung on 1/13/26.
+//
+
+import SwiftUI
+
+struct DropItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropItem()
+}
