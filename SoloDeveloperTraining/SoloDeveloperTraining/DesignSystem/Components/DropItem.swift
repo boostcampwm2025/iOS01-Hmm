c26376ad1caa57c82f8ae3af61fe4d565629d58a
JunYoung <ghddns34@gmail.com>
2026-01-13 11:19:55 +0900

    feat: dropItem 구현

--- SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/DropItem.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/DropItem.swift
@@ -7,12 +7,44 @@
 
 import SwiftUI
 
+private enum Constant {
+    static let itemSize = CGSize(width: 24, height: 24)
+}
+
 struct DropItem: View {
+
+    let type: DropItemType
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(type.imageResource)
+            .resizable()
+            .frame(width: Constant.itemSize.width, height: Constant.itemSize.height)
+    }
+}
+
+extension DropItem {
+    enum DropItemType {
+        case smallGold
+        case largeGold
+        case error
+
+        var imageResource: ImageResource {
+            switch self {
+            case .smallGold:
+                return .dropItemSmallGold
+            case .largeGold:
+                return .dropItemLargeGold
+            case .error:
+                return .dropItemError
+            }
+        }
     }
 }
 
 #Preview {
-    DropItem()
+    HStack {
+        DropItem(type: .smallGold)
+        DropItem(type: .largeGold)
+        DropItem(type: .error)
+    }
 }
