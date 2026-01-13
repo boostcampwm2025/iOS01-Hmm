ae435ff59be392efbf062381582d028ae7e3e987
Tom Bum Su Choi <qjatn0545@naver.com>
2026-01-14 04:38:04 +0900

    feat: 테스트용 뷰 추가

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/Devlopment/Presentation/StackGameTestView.swift
@@ -0,0 +1,79 @@
+//
+//  StackGameTestView.swift
+//  SoloDeveloperTraining
+//
+//  Created by 최범수 on 2026-01-13.
+//
+
+import SpriteKit
+import SwiftUI
+
+@Observable
+final class StackGameTestViewModel {
+    let game: StackGame
+
+    var feverSystem: FeverSystem { game.feverSystem }
+    var gold: Int { game.user.wallet.gold }
+    var diamond: Int { game.user.wallet.diamond }
+
+    var coffeeCount: Int {
+        game.user.inventory.count(.coffee) ?? 0
+    }
+
+    var energyDrinkCount: Int {
+        game.user.inventory.count(.energyDrink) ?? 0
+    }
+
+    init(game: StackGame) {
+        self.game = game
+    }
+
+    func stopGame() {}
+
+    func useCoffee() {
+        game.buffSystem.useConsumableItem(type: .coffee)
+    }
+
+    func useEnergyDrink() {
+        game.buffSystem.useConsumableItem(type: .energyDrink)
+    }
+}
+
+struct StackGameTestView: View {
+    @State var viewModel: StackGameTestViewModel
+
+    private let scene: StackGameScene
+
+    init(user: User, calculator: Calculator) {
+        let game = StackGame(user: user, calculator: calculator)
+        let scene = StackGameScene(stackGame: game)
+
+        self.scene = scene
+        self._viewModel = State(wrappedValue: .init(game: game))
+    }
+
+    var body: some View {
+        VStack {
+            VStack {
+                Text("골드: \(viewModel.gold)")
+                Text("다이아몬드: \(viewModel.diamond)")
+            }
+            .frame(maxHeight: .infinity)
+
+            ZStack(alignment: .top) {
+                SpriteView(scene: scene)
+
+                GameToolBar(
+                    closeButtonDidTapHandler: viewModel.stopGame,
+                    coffeeButtonDidTapHandler: viewModel.useCoffee,
+                    energyDrinkButtonDidTapHandler: viewModel.useEnergyDrink,
+                    feverState: viewModel.feverSystem,
+                    coffeeCount: .constant(viewModel.coffeeCount),
+                    energyDrinkCount: .constant(viewModel.energyDrinkCount)
+                )
+                .padding()
+            }
+            .frame(maxHeight: .infinity)
+        }
+    }
+}
