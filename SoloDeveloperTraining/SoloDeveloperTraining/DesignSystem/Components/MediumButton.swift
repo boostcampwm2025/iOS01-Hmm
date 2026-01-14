238f59c9dcddf11405b0bc5721cb41e93b447c7c
Tom Bum Su Choi <qjatn0545@naver.com>
2026-01-15 02:59:03 +0900

    feat: MediumButton 구현

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/MediumButton.swift
@@ -0,0 +1,104 @@
+//
+//  MediumButton.swift
+//  SoloDeveloperTraining
+//
+//  Created by 최범수 on 2026-01-14.
+//
+
+import SwiftUI
+
+private enum Constant {
+    static let radius: CGFloat = 8
+
+    enum Size {
+        static let buttonWidth: CGFloat = 89
+        static let buttonHeight: CGFloat = 44
+        static let badgeWidth: CGFloat = 30
+        static let badgeHeight: CGFloat = 30
+    }
+
+    enum Offset {
+        static let badgeOffsetX: CGFloat = 17
+        static let badgeOffsetY: CGFloat = -15
+    }
+
+    enum Opacity {
+        static let pressed: Double = 0.8
+        static let unPressed: Double = 1.0
+    }
+}
+
+struct MediumButton: View {
+    @State private var isPressed: Bool = false
+
+    let title: String
+    var fillStyle: Bool = false
+    var hasBadge: Bool = false
+    var isEnabled: Bool = true
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.pfFont(.caption))
+                .foregroundColor(fillStyle ? .white : .black)
+                .frame(width: Constant.Size.buttonWidth, height: Constant.Size.buttonHeight)
+                .background(backgroundColor)
+                .cornerRadius(Constant.radius)
+                .opacity(isPressed ? Constant.Opacity.pressed : Constant.Opacity.unPressed)
+                .overlay {
+                    if !fillStyle {
+                        RoundedRectangle(cornerRadius: Constant.radius)
+                            .stroke()
+                    }
+                }
+                .overlay(alignment: .topTrailing) {
+                    if hasBadge { badge }
+                }
+        }
+        .disabled(!isEnabled)
+        .buttonStyle(.pressable(isPressed: $isPressed))
+    }
+
+    private var backgroundColor: Color {
+        if !isEnabled {
+            return .gray200
+        }
+        return fillStyle ? .orange500 : .white
+    }
+
+    private var badge: some View {
+        Image(.iconDiamondPlus)
+            .resizable()
+            .frame(width: Constant.Size.badgeWidth, height: Constant.Size.badgeHeight)
+            .offset(x: Constant.Offset.badgeOffsetX, y: Constant.Offset.badgeOffsetY)
+    }
+}
+
+#Preview {
+    VStack(spacing: 20) {
+        MediumButton(title: "버튼 텍스트", fillStyle: true) {
+            print("버튼 클릭1")
+        }
+
+        MediumButton(title: "버튼 텍스트", fillStyle: true, isEnabled: false) {
+            print("버튼 클릭2")
+        }
+
+        MediumButton(title: "버튼 텍스트", fillStyle: true, hasBadge: true) {
+            print("버튼 클릭3")
+        }
+
+        MediumButton(title: "버튼 텍스트", fillStyle: true, hasBadge: true, isEnabled: false) {
+            print("버튼 클릭4")
+        }
+
+        MediumButton(title: "버튼 텍스트", fillStyle: false) {
+            print("버튼 클릭5")
+        }
+
+        MediumButton(title: "버튼 텍스트", fillStyle: false, hasBadge: true) {
+            print("버튼 클릭6")
+        }
+    }
+}
