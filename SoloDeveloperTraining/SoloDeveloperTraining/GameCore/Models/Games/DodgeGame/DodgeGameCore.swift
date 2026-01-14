b4d3d01ca1624b493c085b1fc2eef17825f98127
JunYoung <ghddns34@gmail.com>
2026-01-14 15:55:05 +0900

    rename: 파일 폴더링 및 fallingitem 분리

--- SoloDeveloperTraining/SoloDeveloperTraining/GameCore/Models/Games/DodgeGame/DodgeGameCore.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/GameCore/Models/Games/DodgeGame/DodgeGameCore.swift
@@ -12,23 +12,6 @@ private enum Constant {
     static let itemRemovalOffset: CGFloat = 50
 }
 
-struct FallingItem: Identifiable {
-    /// 고유 식별자
-    let id = UUID()
-    /// 아이템 타입 (smallGold, largeGold, bug)
-    let type: DropItem.DropItemType
-    /// 화면 상의 위치 (중심점 기준)
-    var position: CGPoint
-    /// 아이템 크기
-    let size: CGSize = CGSize(width: 24, height: 24)
-
-    /// 아이템 위치를 업데이트
-    /// - Parameter offset: Y축 이동 오프셋
-    mutating func updatePosition(by offset: CGFloat) {
-        position.y += offset
-    }
-}
-
 @Observable
 final class DodgeGameCore {
     // MARK: - 게임 설정
