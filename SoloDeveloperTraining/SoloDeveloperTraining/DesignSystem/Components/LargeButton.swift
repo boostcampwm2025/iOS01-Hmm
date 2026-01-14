31211656503610c5d60eeaab78be8c62c5fe49c7
Tom Bum Su Choi <qjatn0545@naver.com>
2026-01-14 22:56:41 +0900

    feat: LargeButtonStyle 구현

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/DesignSystem/Components/LargeButton.swift
@@ -0,0 +1,81 @@
+//
+//  LargeButton.swift
+//  SoloDeveloperTraining
+//
+//  Created by 최범수 on 2026-01-14.
+//
+
+import SwiftUI
+
+private enum Constant {
+    static let radius: CGFloat = 8
+
+    enum Size {
+        static let buttonWidth: CGFloat = 170
+        static let buttonHeight: CGFloat = 46
+        static let badgeWidth: CGFloat = 30
+        static let badgeHeight: CGFloat = 30
+
+    }
+
+    enum Offset {
+        static let badgeOffsetX: CGFloat = 17
+        static let badgeOffsetY: CGFloat = -15
+    }
+
+    enum Opacity {
+        static let pressed: Double = 0.8
+        static let unPressed: Double = 1.0
+    }
+}
+
+struct LargeButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
+    var hasBadge: Bool = false
+    var backgroundColor: Color { isEnabled ? .orange500 : .gray200 }
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.pfFont(.body))
+            .foregroundColor(.white)
+            .frame(width: Constant.Size.buttonWidth, height: Constant.Size.buttonHeight)
+            .background(backgroundColor)
+            .cornerRadius(Constant.radius)
+            .opacity(configuration.isPressed ? Constant.Opacity.pressed : Constant.Opacity.unPressed)
+            .overlay(alignment: .topTrailing) {
+                if hasBadge { badge }
+            }
+    }
+
+    private var badge: some View {
+        Image(.iconDiamondPlus)
+            .resizable()
+            .frame(width: Constant.Size.badgeWidth, height: Constant.Size.badgeHeight)
+            .offset(x: Constant.Offset.badgeOffsetX, y: Constant.Offset.badgeOffsetY)
+    }
+}
+
+#Preview {
+    Button("버튼 텍스트") {
+        print("버튼 클릭1")
+    }
+    .buttonStyle(LargeButtonStyle(hasBadge: false))
+
+    Button("버튼 텍스트") {
+        print("버튼 클릭2")
+    }
+    .buttonStyle(LargeButtonStyle(hasBadge: false))
+    .disabled(true)
+
+    Button("버튼 텍스트") {
+        print("버튼 클릭3")
+    }
+    .buttonStyle(LargeButtonStyle(hasBadge: true))
+    .disabled(true)
+
+    Button("버튼 텍스트") {
+        print("버튼 클릭4")
+    }
+    .buttonStyle(LargeButtonStyle(hasBadge: true))
+}
