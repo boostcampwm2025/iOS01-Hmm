87cd351b030339121214969b363f7d38270b02de
JunYoung <ghddns34@gmail.com>
2026-01-19 21:22:11 +0900

    chore: 파일 분리

--- SoloDeveloperTraining/SoloDeveloperTraining/GameCore/Models/Systems/ShopSystem/ShopSystem.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/GameCore/Models/Systems/ShopSystem/ShopSystem.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-/// 상점 시스템 관련 에러
-enum ShopSystemError: Error {
-    /// 골드 부족
-    case insufficientGold
-    /// 다이아몬드 부족
-    case insufficientDiamond
-    /// 구매 실패
-    case purchaseFailed
-}
-
 /// 상점 시스템 관리 클래스
 final class ShopSystem {
 
@@ -117,28 +107,4 @@ final class ShopSystem {
         user.wallet.spendGold(item.cost.gold)
         user.wallet.spendDiamond(item.cost.diamond)
     }
-}
-
-/// 상점 아이템 모델
-struct Item: Identifiable {
-    /// 아이템 고유 ID
-    var id: UUID = .init()
-    /// 아이템 제목
-    let title: String
-    /// 아이템 설명
-    let description: String
-    /// 아이템 가격
-    let cost: Cost
-    /// 아이템 타입
-    let type: ItemType
-}
-
-/// 아이템 타입 구분
-enum ItemType {
-    /// 장비 아이템
-    case equipment(Equipment)
-    /// 소비 아이템
-    case consumable(Consumable)
-    /// 부동산 아이템
-    case housing(Housing)
 }
