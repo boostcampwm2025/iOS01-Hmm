ec3a5ad56b00e42ce9e1cb9fb7341a7f8f9ddff5
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-01-21 16:33:10 +0900

    feat: 미션 뷰 생성

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/MissionView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionView.swift
+//  SoloDeveloperTraining
+//
+//  Created by sunjae on 1/21/26.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    private let user: User
+
+    init(user: User) {
+        self.user = user
+    }
+
+    var body: some View {
+        Text("미션 뷰")
+    }
+}
