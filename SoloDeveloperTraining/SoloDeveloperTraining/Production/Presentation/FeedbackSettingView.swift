bd71fadfa483e1b6ebff9201100c924192b4d8dc
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-01-23 02:23:41 +0900

    fix: 피드백 버튼 아이콘 변경

--- SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/FeedbackSettingView.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/FeedbackSettingView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 private enum Constant {
     static let iconSize: CGFloat = 38
     static let horizontalSpacing: CGFloat = 12
-    static let leadingPadding: CGFloat = iconSize + horizontalSpacing + 30
-    static let bottomPadding: CGFloat = 15
 
     enum Opacity {
         static let imageEnabled: Double = 1
@@ -20,9 +18,9 @@ private enum Constant {
 
     enum ImageName {
         static let soundActive: String = "speaker.circle.fill"
-        static let soundInactive: String = "speaker.slash.circle.fill"
-        static let hapticActive: String = "antenna.radiowaves.left.and.right.circle.fill"
-        static let hapticInactive: String = "antenna.radiowaves.left.and.right.slash.circle.fill"
+        static let soundInactive: String = "speaker.circle"
+        static let hapticActive: String = "iphone.gen1.radiowaves.left.and.right.circle.fill"
+        static let hapticInactive: String = "iphone.gen1.radiowaves.left.and.right.circle"
     }
 }
 
@@ -46,6 +44,7 @@ struct FeedbackSettingView: View {
             // 햅틱 버튼
             Button {
                 HapticService.shared.toggle()
+                
             } label: {
                 Image(
                     systemName: HapticService.shared.isEnabled ? Constant.ImageName.hapticActive : Constant.ImageName.hapticInactive
@@ -58,7 +57,5 @@ struct FeedbackSettingView: View {
                 )
             }
         }
-        .padding(.leading, Constant.leadingPadding)
-        .padding(.bottom, Constant.bottomPadding)
     }
 }
