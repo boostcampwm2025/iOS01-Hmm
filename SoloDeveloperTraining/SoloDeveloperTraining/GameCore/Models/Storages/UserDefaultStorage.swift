e467a1b64b0baff0c241f6aeeb577b9ff826cda6
JunYoung <ghddns34@gmail.com>
2026-01-27 12:17:56 +0900

    feat: userDefaultStorage 생성

---
+++ SoloDeveloperTraining/SoloDeveloperTraining/GameCore/Models/Storages/UserDefaultStorage.swift
@@ -0,0 +1,22 @@
+//
+//  UserDefaultStorage.swift
+//  SoloDeveloperTraining
+//
+//  Created by SeoJunYoung on 1/27/26.
+//
+
+import Foundation
+
+struct UserDefaultStorage: KeyValueStorage {
+    func set(key: String, value: Any) {
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    func integer(key: String) -> Int {
+        return UserDefaults.standard.integer(forKey: key)
+    }
+    
+    func register(defaults: [String: Any]) {
+        UserDefaults.standard.register(defaults: defaults)
+    }
+ }
