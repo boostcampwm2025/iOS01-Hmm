760fc198be2f813758d6d5fc6bf13aa446809e51
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-05-27 21:25:03 +0900

    feat: 불필요한 코드 제거 및 광고 단위 id 분기 추가

--- SoloDeveloperTraining/SoloDeveloperTraining/Production/Advertisement/InterstitialAdUnit.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Advertisement/InterstitialAdUnit.swift
@@ -9,9 +9,15 @@ import GoogleMobileAds
 
 final class InterstitialAdUnit: NSObject, AdUnit {
     private var interstitialAd: InterstitialAd?
-    private var adUnitID: String = "ca-app-pub-3940256099942544/4411468910"
-    private var continuation: CheckedContinuation<Void, Never>?
     private var resultContinuation: CheckedContinuation<Bool, Never>?
+
+    var adUnitID: String {
+        #if DEBUG
+            "ca-app-pub-3940256099942544/4411468910"
+        #else
+            "ca-app-pub-xxxxxxxxxxxxxxxx/xxxxxxxxxx"  // 실제 ID
+        #endif
+    }
 
     var isReady: Bool {
         return interstitialAd != nil
@@ -25,28 +31,12 @@ final class InterstitialAdUnit: NSObject, AdUnit {
         interstitialAd?.fullScreenContentDelegate = self
     }
 
-    func show() async {
-        guard let interstitialAd else {
-            print("❌ Ad not ready")
-            return
-        }
-        guard continuation == nil else {
-            print("⚠️ Ad is already showing")
-            return
-        }
-
-        await withCheckedContinuation { continuation in
-            self.continuation = continuation
-            interstitialAd.present(from: nil)
-        }
-    }
-
     func showWithResult() async -> Bool {
         guard let interstitialAd else {
             print("❌ Ad not ready")
             return false
         }
-        guard continuation == nil, resultContinuation == nil else {
+        guard resultContinuation == nil else {
             print("⚠️ Ad is already showing")
             return false
         }
@@ -66,8 +56,6 @@ extension InterstitialAdUnit: FullScreenContentDelegate {
         didFailToPresentFullScreenContentWithError error: Error
     ) {
         print("\(#function) called")
-        continuation?.resume()
-        continuation = nil
         resultContinuation?.resume(returning: false)
         resultContinuation = nil
         interstitialAd = nil
@@ -76,8 +64,6 @@ extension InterstitialAdUnit: FullScreenContentDelegate {
     // 광고 화면이 닫혔을 경우
     func adDidDismissFullScreenContent(_ ads: FullScreenPresentingAd) {
         print("\(#function) called")
-        continuation?.resume()
-        continuation = nil
         resultContinuation?.resume(returning: true)
         resultContinuation = nil
         interstitialAd = nil
