28611c92a7fc2e2c3bc597fb0a28fc43902cc68a
snughnu <ksnughnu@gmail.com>
2026-06-12 01:18:55 +0900

    feat: design example popup view 타입 추가

--- SoloDeveloperTraining/DUDesignSystemExample/Component/ComponentPopupView.swift
+++ SoloDeveloperTraining/DUDesignSystemExample/Component/ComponentPopupView.swift
@@ -10,7 +10,7 @@ struct ComponentPopupView: View {
 
     @State private var selectedType: Int = 0
 
-    private let typeLabels = ["Notice", "Confirm", "Reward"]
+    private let typeLabels = ["Notice", "Confirm", "Reward", "AdBonus", "AdReward", "AdConfirm"]
 
     var body: some View {
         VStack(spacing: 0) {
@@ -34,7 +34,7 @@ struct ComponentPopupView: View {
                             cancelAction: {},
                             confirmAction: {}
                         ))
-                    default:
+                    case 2:
                         Popup(type: .reward(
                             title: "팝업 타이틀",
                             body: "팝업 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용\n내용 내용",
@@ -43,6 +43,42 @@ struct ComponentPopupView: View {
                             rewardTrailingText: "20",
                             buttonText: "닫기",
                             action: {}
+                        ))
+                    case 3:
+                        Popup(type: .adBonus(
+                            title: "보너스",
+                            body: "퀴즈 풀이를 완료했습니다!\n진정한 개발자에 한 걸음 더 가까워졌습니다.",
+                            rewardLeadingText: "획득한 다이아 : ",
+                            rewardIcon: .diamond,
+                            rewardTrailingText: "5",
+                            cancelText: "취소",
+                            adIcon: .ad,
+                            adText: "2배 얻기",
+                            cancelAction: {},
+                            adAction: {}
+                        ))
+                    case 4:
+                        Popup(type: .adReward(
+                            title: "타이틀",
+                            body: "안녕하세요\n이곳은 팝업 내용을 적는 곳입니다",
+                            cancelText: "취소",
+                            adIcon: .ad,
+                            adText: "2배 얻기",
+                            cancelAction: {},
+                            adAction: {}
+                        ))
+                    default:
+                        Popup(type: .adConfirm(
+                            title: "아이템구매",
+                            body: "[₩2,000,000]을 사용하여\n구매하시겠습니까?",
+                            subText: "(성공 확률: 70%)",
+                            cancelText: "취소",
+                            adIcon: .ad,
+                            adText: "확률 UP",
+                            confirmText: "구매",
+                            cancelAction: {},
+                            adAction: {},
+                            confirmAction: {}
                         ))
                     }
                 }
