9836c2539b3061ebaaada9547a81c24e6753b6e6
snughnu <ksnughnu@gmail.com>
2026-06-15 23:30:22 +0900

    fix: TextButton 원복

--- DUDesignSystem/Sources/DUDesignSystem/Components/Buttons/TextButton.swift
+++ DUDesignSystem/Sources/DUDesignSystem/Components/Buttons/TextButton.swift
@@ -27,7 +27,6 @@ public struct TextButton: View {
     }
 
     public var text: String
-    public var icon: DUIconName?
     public var type: TextButtonType
     public var size: TextButtonSize
     public var state: TextButtonState
@@ -38,7 +37,6 @@ public struct TextButton: View {
 
     public init(
         text: String,
-        icon: DUIconName? = nil,
         type: TextButtonType,
         size: TextButtonSize = .large,
         state: TextButtonState = .default,
@@ -46,7 +44,6 @@ public struct TextButton: View {
         action: @escaping () -> Void
     ) {
         self.text = text
-        self.icon = icon
         self.type = type
         self.size = size
         self.state = state
