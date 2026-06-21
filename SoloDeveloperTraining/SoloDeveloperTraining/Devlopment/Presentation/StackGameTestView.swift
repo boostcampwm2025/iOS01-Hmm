b7754a20b3bd9789e6b8b09e584f505d27c4e527
snughnu <ksnughnu@gmail.com>
2026-06-22 02:42:49 +0900

    remove: 구형 컴포넌트 제거

--- SoloDeveloperTraining/SoloDeveloperTraining/Devlopment/Presentation/StackGameTestView.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Devlopment/Presentation/StackGameTestView.swift
@@ -8,6 +8,8 @@
 import SpriteKit
 import SwiftUI
 
+import DUDesignSystem
+
 struct StackGameTestView: View {
     @State private var game: StackGame
     private let scene: StackGameScene
@@ -31,14 +33,24 @@ struct StackGameTestView: View {
             ZStack(alignment: .top) {
                 SpriteView(scene: scene)
 
-                OldGameToolBar(
-                    closeButtonDidTapHandler: stopGame,
-                    coffeeButtonDidTapHandler: useCoffee,
-                    energyDrinkButtonDidTapHandler: useEnergyDrink,
-                    feverState: game.feverSystem,
-                    buffSystem: game.buffSystem,
-                    coffeeCount: .constant(game.user.inventory.count(.coffee) ?? 0),
-                    energyDrinkCount: .constant(game.user.inventory.count(.energyDrink) ?? 0)
+                GameToolBar(
+                    feverStage: game.feverSystem.feverStage,
+                    feverProgress: {
+                        let stageBase = Double(game.feverSystem.feverStage) * 100.0
+                        return (game.feverSystem.feverPercent - stageBase) / 100.0
+                    }(),
+                    feverMultiplier: game.feverSystem.feverStage == 0 ? 0 : game.feverSystem.feverMultiplier,
+                    coffeeCount: game.user.inventory.count(.coffee) ?? 0,
+                    energyDrinkCount: game.user.inventory.count(.energyDrink) ?? 0,
+                    coffeeCooldown: {
+                        Double(game.buffSystem.coffeeDuration) / Double(ConsumableType.coffee.duration)
+                    }(),
+                    energyDrinkCooldown: {
+                        Double(game.buffSystem.energyDrinkDuration) / Double(ConsumableType.energyDrink.duration)
+                    }(),
+                    onClose: stopGame,
+                    onCoffee: useCoffee,
+                    onEnergyDrink: useEnergyDrink
                 )
                 .padding()
             }
