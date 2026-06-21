219424b7ac71c57a975ce115b143e5ac67af076d
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-06-21 22:19:30 +0900

    fix: 데이터 로드 백그라운드 실행

--- SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/GIFView.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/GIFView.swift
@@ -10,43 +10,44 @@ struct GIFView: UIViewRepresentable {
     func makeUIView(context: Context) -> UIImageView {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
-        if let duration = loadGIF(into: imageView) {
-            DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                onFinished?()
-            }
-        }
+        loadGIF(into: imageView)
 
         return imageView
     }
 
-    func updateUIView(_ uiView: UIImageView, context: Context) {}
+    func updateUIView(_ uiView: UIImageView, context: Context) {
+        loadGIF(into: uiView)
+    }
 
-    // SwiftUI 상위 뷰에서 지정한 사이즈 적용
     func sizeThatFits(_ proposal: ProposedViewSize, uiView: UIImageView, context: Context) -> CGSize? {
         guard let image = uiView.image else { return nil }
-
-        let imageSize = image.size
-        guard imageSize.width > 0 else { return nil }
+        guard image.size.width > 0 else { return nil }
 
         let width = proposal.width ?? UIScreen.main.bounds.width
-        let height = width * imageSize.height / imageSize.width
+        let height = width * image.size.height / image.size.width
         return CGSize(width: width, height: height)
     }
 
-    private func loadGIF(into imageView: UIImageView) -> TimeInterval? {
-        guard
-            let url = DUGIF.bundle.url(
-                forResource: gifName.replacingOccurrences(of: ".gif", with: ""),
-                withExtension: "gif"
-            ),
-            let animatedGIF = UIImage.animatedGIF(url: url)
-        else {
-            imageView.image = nil
-            return nil
-        }
+    private func loadGIF(into imageView: UIImageView) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard
+                let url = DUGIF.bundle.url(forResource: gifName.replacingOccurrences(of: ".gif", with: ""), withExtension: "gif"),
+                let animatedGIF = UIImage.animatedGIF(url: url)
+            else {
+                return
+            }
 
-        imageView.image = animatedGIF.image
-        return animatedGIF.duration
+            DispatchQueue.main.async {
+                imageView.image = animatedGIF.image
+
+                if onFinished != nil {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + animatedGIF.duration) {
+                        self.onFinished?()
+                    }
+                }
+            }
+        }
     }
 }
