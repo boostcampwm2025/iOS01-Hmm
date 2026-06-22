933278c99bef54f422f01cc70dd30a1145e21e49
snughnu <ksnughnu@gmail.com>
2026-06-22 20:57:55 +0900

    remove: slate icon 제거

--- DUDesignSystem/Sources/DUDesignSystem/Components/DUIcon.swift
+++ DUDesignSystem/Sources/DUDesignSystem/Components/DUIcon.swift
@@ -27,7 +27,6 @@ public enum DUIconName: String, CaseIterable {
     case share       = "iconShare"
     case dogFace     = "iconDogFace"
     case dogFoot     = "iconDogFoot"
-    case movieSlate  = "iconMovieSlate"
 }
 
 public struct DUIcon: View {
