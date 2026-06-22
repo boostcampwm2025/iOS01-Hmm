e27aee8ada4b459b273c46f2d610ff9f5410cd39
JunYoung <ghddns34@gmail.com>
2026-06-23 01:37:25 +0900

    refactor: MissionView를 DUDesignSystem 컴포넌트로 전환
    - DUDesignSystem.MissionCard, ProgressBar 적용
    - 프로그래스바 위에 caption 텍스트 ZStack 오버레이
    - 3열 고정 그리드, 카드 간격 xs(4), 줄 간격 md(16), 토큰 패딩 적용
    - MissionLevel/MissionCardState 매핑 extension DisplayStateExtensions로 이동

--- SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/DisplayStateExtensions.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/DisplayStateExtensions.swift
@@ -24,6 +24,27 @@ extension ItemState {
     }
 }
 
+extension MissionLevel {
+    var trophyType: DUDesignSystem.MissionCard.MissionTrophyType {
+        switch self {
+        case .gold:    return .gold
+        case .silver:  return .silver
+        case .bronze:  return .bronze
+        case .special: return .special
+        }
+    }
+}
+
+extension MissionCardState {
+    var missionCardState: DUDesignSystem.MissionCard.MissionCardState {
+        switch self {
+        case .claimed:                              return .claimed
+        case .claimable:                            return .claimable
+        case .inProgress(let current, let total):   return .inProgress(current: current, total: total)
+        }
+    }
+}
+
 extension Cost {
     var itemButtonType: ItemButton.ItemButtonType {
         if gold > 0 && diamond > 0 {
