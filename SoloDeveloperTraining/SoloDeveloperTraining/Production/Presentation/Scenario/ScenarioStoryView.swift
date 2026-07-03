bf395d5a657594be174fd37141b9a0ca3d794596
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2026-07-03 15:08:41 +0900

    fix: 시나리오 페이지 ignoreSafeArea 추가

--- SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/Scenario/ScenarioStoryView.swift
+++ SoloDeveloperTraining/SoloDeveloperTraining/Production/Presentation/Scenario/ScenarioStoryView.swift
@@ -44,7 +44,7 @@ struct ScenarioStoryView: View {
 
     var body: some View {
         ZStack {
-            VStack(spacing: isEnding ? TokenSpacing.xl : TokenSpacing.lg) {
+            VStack(spacing: TokenSpacing.lg) {
                 if isEnding { endingResultView }
 
                 if let ending = finalEnding {
@@ -54,6 +54,7 @@ struct ScenarioStoryView: View {
                         imageName: ending.type.imageName
                     )
                     .id("ending")
+                    .padding(.bottom, TokenSpacing.sm)
                 } else if let page = manager.currentPage {
                     StoryCard(
                         type: .levelUp,
@@ -100,6 +101,7 @@ struct ScenarioStoryView: View {
         }
         .duPopup(isPresented: isShareSheetPresented) { shareSheetPopup }
         .duPopup(isPresented: isRebirthConfirmPopupPresented) { rebirthConfirmPopupView }
+        .ignoresSafeArea()
     }
 }
 
